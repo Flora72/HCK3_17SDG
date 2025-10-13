ccd1ee220aa001a0dcf39e12db9cf975388e717a
Flora72 <zahratheo569@gmail.com>
2025-10-13 10:23:59 +0300

    static

--- AgroApp/settings.py
+++ AgroApp/settings.py
@@ -114,12 +114,16 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+CSRF_TRUSTED_ORIGINS = ['https://agromatch-iyw0.onrender.com']
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
