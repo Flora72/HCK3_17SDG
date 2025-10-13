c562603452e77452e317772db15d10e3edfcd34f
Flora72 <zahratheo569@gmail.com>
2025-10-13 10:16:47 +0300

    allowed hosts

--- AgroApp/settings.py
+++ AgroApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['agromatch.onrender.com']
+ALLOWED_HOSTS = ['agromatch-iyw0.onrender.com']
 
 
 
